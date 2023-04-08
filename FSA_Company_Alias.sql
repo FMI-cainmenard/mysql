3a5ea93bccf444527860a2ecaf2360b17da238ac
FMI-cainmenard <126827920+FMI-cainmenard@users.noreply.github.com>
2023-04-07 18:19:06 -0600

    replaced SF_account with FSA_Main

--- FSA_Company_Alias.sql
+++ FSA_Company_Alias.sql
@@ -1,28 +1,27 @@
 USE Insights;
 
 -- Drop if exists to recreate
-DROP TABLE IF EXISTS Company_Alias;
+DROP TABLE IF EXISTS FSA_Company_Alias;
 
 -- Create a new table to store the company aliases
-CREATE TABLE Company_Alias (
+CREATE TABLE FSA_Company_Alias (
     Data_Key__c VARCHAR(255) PRIMARY KEY,
     Alias VARCHAR(255) NOT NULL
 );
 
 -- Insert unique company IDs and their corresponding random aliases into the new table
-INSERT INTO Company_Alias (Data_Key__c, Alias)
+INSERT INTO FSA_Company_Alias (Data_Key__c, Alias)
 SELECT DISTINCT
     a.Data_Key__c,
     CONCAT(CHAR(FLOOR(RAND() * 26) + 65),CHAR(FLOOR(RAND() * 26) + 97),CAST(FLOOR(RAND() * 10) AS CHAR(1))) AS Alias
 FROM
-    SF_account a
+    FSA_Main a
 LEFT JOIN
-    SF_peer_groups b
-    ON a.Peer_Group__c = b.Id
-LEFT JOIN
-    Company_Alias c
+    FSA_Company_Alias c
     ON a.Data_Key__c = c.Data_Key__c
 WHERE c.Data_Key__c IS NULL
     AND a.Data_Key__c IS NOT NULL
 GROUP BY
     a.Data_Key__c;
+
+SELECT * FROM FSA_Company_Alias;
